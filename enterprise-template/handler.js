f04f6a457e7dae6c7dc111d5e3e133445a3491db
Jonathan Glock <glockjt@gmail.com>
2019-03-01 14:07:04 -0800

    Initial commit.

---
+++ enterprise-template/handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const loremIpsum = require('lorem-ipsum');
+
+module.exports.hello = async (event, context) => {
+  const output = loremIpsum();
+  let body;
+  if (event.body) {
+    body = JSON.parse(body);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: output,
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
